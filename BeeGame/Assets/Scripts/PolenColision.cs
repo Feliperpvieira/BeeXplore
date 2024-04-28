a1bb3c6ae4df328f387873bc37550c02959db9d6
Felipe <47862856+Feliperpvieira@users.noreply.github.com>
2024-04-28 18:11:27 +0100

    add efeito

--- BeeGame/Assets/Scripts/PolenColision.cs
+++ BeeGame/Assets/Scripts/PolenColision.cs
@@ -6,11 +6,15 @@ public class PolenColision : MonoBehaviour
 {
 
     public int points;
+    [Header("Partículas emitidias")]
+    public GameObject particulasPontos;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            //Instantiate(particulasPontos, transform.position, Quaternion.identity);
+
             gameObject.SetActive(false); //desativa o game object atual
             Pontuacao.score = Pontuacao.score + points;
         }
