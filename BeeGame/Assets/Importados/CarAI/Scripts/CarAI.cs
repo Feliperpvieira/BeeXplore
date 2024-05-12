603f112a84fd3e8e80dff0e3fb3756476b5eb206
Felipe <47862856+Feliperpvieira@users.noreply.github.com>
2024-05-12 16:56:40 +0100

    update city

--- BeeGame/Assets/Importados/CarAI/Scripts/CarAI.cs
+++ BeeGame/Assets/Importados/CarAI/Scripts/CarAI.cs
@@ -38,7 +38,7 @@ public class CarAI : MonoBehaviour
 
     private Vector3 PostionToFollow = Vector3.zero;
     private int currentWayPoint;
-    private float AIFOV = 60;
+    private float AIFOV = 75; //used to be 60
     private bool allowMovement;
     private int NavMeshLayerBite;
     private List<Vector3> waypoints = new List<Vector3>();
