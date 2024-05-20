bd4edf9c7d3bbadd0d3a627eacff59896331b15e
Felipe <47862856+Feliperpvieira@users.noreply.github.com>
2024-05-20 16:41:14 +0100

    add win condition

--- BeeGame/Assets/Scripts/Pontuacao.cs
+++ BeeGame/Assets/Scripts/Pontuacao.cs
@@ -11,16 +11,31 @@ public class Pontuacao : MonoBehaviour
     public int scoreToWin; //max value, pontuacao pra ganhar a fase
     public TMP_Text scoreText; //Pontuacao do jogador
     public Image barraPolen;
+    public GameObject victoryScreen;
+    public GameObject botoes;
 
     void Start()
     {
         score = 0; //Reseta a contagem no início de partidas
+        victoryScreen.SetActive(false);
+        scoreText.enabled = true;
+        barraPolen.enabled = true;
+        botoes.SetActive(true);
     }
 
     public void Update()
     {
         scoreText.text = score.ToString();
         GetCurrentFill();
+
+        if(score >= scoreToWin)
+        {
+            victoryScreen.SetActive(true);
+            scoreText.enabled = false;
+            barraPolen.enabled = false;
+            botoes.SetActive(false);
+        }
+
     }
 
     void GetCurrentFill() // Funcao que faz passar o tempo na barra de progresso, ao lado do cronometro
